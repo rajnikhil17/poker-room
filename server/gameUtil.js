4747592d158206de52e93ea88f444d273fec2c49
Shania Jain <shaniajain@Shanias-MacBook-Air.local>
2020-07-20 14:45:55 -0700

    fixed fold action

--- server/gameUtil.js
+++ server/gameUtil.js
@@ -120,7 +120,7 @@ const setInitialBlinds = () => {
 		else if (d+1 >= gameState.players.length) {
 			gameState.players[0].smallBlind = true;
 		}
-		
+
 		//set big blind
 		if (d+2 < gameState.players.length && gameState.players[d+1].view === false) {
 			gameState.players[d+2].bigBlind = true;
@@ -228,6 +228,57 @@ const moveBlinds = () => {
 };
 
 const check = (socketId) => {
+	console.log("IN HERE");
+	var i;
+	for (i = 0; i < gameState.players.length; i++) {
+		if (gameState.players[i].id === socketId) {
+			break;
+		}
+	}
+	if(gameState.players[i].view === false) {
+		gameState.players[i].action = true;
+		for (let j = 0; j < gameState.players.length; j++) {
+			gameState.players[j].active = false;
+		}
+		var count = 1;
+		while((i + count) <= gameState.players.length) {
+				console.log("i = " + i + ", count = " + count);
+				if((i+count) === gameState.players.length) {
+					i = 0;
+					count = 0;
+				}
+				else if(gameState.players[i + count].view === true) {
+					count = count + 1;
+				}
+				else {
+					gameState.players[i+count].active = true;
+					break;
+				}
+		}
+	}
+	else {
+		for (let j = 0; j < gameState.players.length; j++) {
+			gameState.players[j].active = false;
+		}
+		var count = 1;
+		while((i + count) <= gameState.players.length) {
+				console.log("i = " + i + ", count = " + count);
+				if((i+count) === gameState.players.length) {
+					i = 0;
+					count = 0;
+				}
+				else if(gameState.players[i + count].view === true) {
+					count = count + 1;
+				}
+				else {
+					gameState.players[i+count].active = true;
+					break;
+				}
+		}
+	}
+
+
+	/*
 	for (let i = 0; i < gameState.players.length; i++) {
 		if (gameState.players[i].id === socketId && gameState.players[i].view === false) {
 		//	if(gameState.players[i].view === false) {
@@ -237,6 +288,8 @@ const check = (socketId) => {
 		//		gameState.players[i].action = false;
 		//	}
 
+
+			/*
 			if (i + 1 < gameState.players.length) {
 				if(gameState.players[i + 1].view === true) {
 					for (let j = 0; j < gameState.players.length; j++) {
@@ -253,15 +306,18 @@ const check = (socketId) => {
 					gameState.players[i + 1].active = true;
 				//	gameState.players[i].active = false;
 				}
-			} else {
+			}
+			else {
 				for (let j = 0; j < gameState.players.length; j++) {
 					gameState.players[j].active = false;
 				}
 				gameState.players[0].active = true;
 			//	gameState.players[i].active = false;
 			}
-		}
-	}
+			*/
+	//	}
+
+	//}
 	for (let i = 0; i < gameState.players.length; i++) {
 		if(gameState.players[i].smallBlind === true) {
 				console.log("smallBlind is " + gameState.players[i].name);
@@ -414,13 +470,37 @@ const removePlayer = (socketId) => {
 };
 
 const fold = (socketId) => {
-	const winner = gameState.players.filter((player) => player.id !== socketId)[0];
-	potToPlayer(winner);
-	dealPlayers();
-	resetPlayerAction();
-	moveBlinds();
+		const player = gameState.players.filter((player) => player.id === socketId)[0];
+		player.cards = [];
+		player.activeBet = 0;
+		player.view = true;
+		check(socketId);
 
-	gameState.minBet = 20
+		var num_active = 0;
+		for(let i = 0; i < gameState.players.length; i++) {
+			if(gameState.players[i].view === false) {
+				num_active = num_active + 1;
+			}
+		}
+
+		if(num_active === 1) {
+			console.log("ONE PLAYER LEFT");
+			for(let i = 0; i < gameState.players.length; i++) {
+				if(gameState.players[i].view === false) {
+					console.log("winner is " + gameState.players[i].name);
+					const winner = gameState.players[i];
+					for(let j = 0; j < gameState.players.length; j++) {
+						gameState.players[j].view = false;
+					}
+			  	potToPlayer(winner);
+			  	dealPlayers();
+			  	resetPlayerAction();
+			  	moveBlinds();
+				  gameState.minBet = 20;
+					break;
+				}
+			}
+		}
 };
 
 const allInMode = () => {
